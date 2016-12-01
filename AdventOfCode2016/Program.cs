81b2cac8d70c228c8bf4b2f52b50468aeaf527f1
Emil Péter Hegyi <emil.hegyi@tappointment.com>
2016-12-01 16:28:13 +0100

    Advent of code - Day 1 / Solution

---
+++ AdventOfCode2016/Program.cs
@@ -0,0 +1,50 @@
+﻿using System; 
+
+namespace AdventOfCode2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3";
+
+            if (RouteCalculator.Calculate("R1, L1, R1, R1, R1") != 1)
+            {
+                Console.WriteLine("Error at case 1!");
+            }
+
+            if (RouteCalculator.Calculate("R1, L1, R1, R1") != 2)
+            {
+                Console.WriteLine("Error at case 2!");
+            }
+
+            if (RouteCalculator.Calculate("R2, L3") != 5)
+            {
+                Console.WriteLine("Error at case 3!");
+            }
+
+            if (RouteCalculator.Calculate("R2, R2, R2") != 2)
+            {
+                Console.WriteLine("Error at case 4!");
+            }
+
+            if (RouteCalculator.Calculate("L2, L2, L2") != 2)
+            {
+                Console.WriteLine("Error at case 5!");
+            }
+
+            if (RouteCalculator.Calculate("R5, L5, R5, R3") != 12)
+            {
+                Console.WriteLine("Error at case 6!");
+            }
+
+            if (RouteCalculator.Calculate("R1, L1, L1, L1") != 0)
+            {
+                Console.WriteLine("Error at case 7!");
+            }
+
+            RouteCalculator.Calculate(input);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode2016/RouteCalculator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2016
+{
+    public static class RouteCalculator
+    {
+        public static int Calculate(string input)
+        {
+            // Initialization and starting position
+            List<Tuple<int, int>> alreadyWereHere = new List<Tuple<int, int>>();
+            alreadyWereHere.Add(new Tuple<int, int>(0, 0));
+            int x = 0;
+            int y = 0;
+            int xMultiplier = 1;
+            int yMultiplier = 0;
+            bool loopFound = false;
+
+            // Splitting the items
+            string[] inputArray = Regex.Split(input, ",");
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = inputArray[i].Trim();
+                char direction = inputArray[i][0];
+                int val = int.Parse(inputArray[i].Trim(inputArray[i][0]));
+
+                if (i % 2 == 0)
+                {
+                    // We on Y
+                    if (xMultiplier == 1)
+                    {
+                        if (direction == 'R')
+                            yMultiplier = 1;
+                        else
+                            yMultiplier = -1;
+                    }
+                    else
+                    {
+                        if (direction == 'R')
+                            yMultiplier = -1;
+                        else
+                            yMultiplier = 1;
+                    }
+
+                    for (int j = 0; j < val; j++)
+                    {
+                        y += yMultiplier;
+                        CheckLoop(alreadyWereHere, ref x, ref y, ref loopFound);
+                    }
+                }
+                else
+                {
+                    // We go on X
+                    if (yMultiplier == 1)
+                    {
+                        if (direction == 'R')
+                            xMultiplier = -1;
+                        else
+                            xMultiplier = 1;
+                    }
+                    else
+                    {
+                        if (direction == 'R')
+                            xMultiplier = 1;
+                        else
+                            xMultiplier = -1;
+                    }
+
+                    for (int j = 0; j < val; j++)
+                    {
+                        x += xMultiplier;
+                        CheckLoop(alreadyWereHere, ref x, ref y, ref loopFound);
+                    }
+
+                }
+            }
+
+            if (x < 0)
+            {
+                x = x * -1;
+            }
+
+            if (y < 0)
+            {
+                y = y * -1;
+            }
+
+            int distance = x + y;
+            Console.WriteLine("Distance to bunny: " + distance);
+
+            return distance;
+        }
+
+        private static void CheckLoop(List<Tuple<int, int>> alreadyWereHere, ref int x, ref int y, ref bool loopFounded)
+        {
+            if (loopFounded)
+            {
+                return;
+            }
+
+            int y1 = y;
+            int x1 = x;
+
+            if (alreadyWereHere.FirstOrDefault(h => h.Item1 == x1 && h.Item2 == y1) != null)
+            {
+                loopFounded = true;
+
+                if (x < 0)
+                {
+                    x1 = x * -1;
+                }
+
+                if (y < 0)
+                {
+                    y1 = y * -1;
+                }
+
+                int di1 = x1 + y1;
+
+                Console.WriteLine("First loop at: " + di1);
+            }
+            else
+            {
+                alreadyWereHere.Add(new Tuple<int, int>(x, y));
+            }
+        }
+    }
+}
