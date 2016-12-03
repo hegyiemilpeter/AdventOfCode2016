a956e38916bb4acc385ed44b9c5b1490b8fcc18d
Emil Péter Hegyi <emil.hegyi@tappointment.com>
2016-12-03 13:31:17 +0100

    Solutions for day 3.

---
+++ AdventOfCode2016_Day3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("..\\..\\adventOfCode2016_day3_input.txt");
+
+            TriangleCalculator.GetTheNumberOfValidTriangles("5 10 25");
+            TriangleCalculator.GetTheNumberOfValidTriangles("2 2 4");
+
+            TriangleCalculator.AdvancedGetTheNumberOfValidTriangles(@"2 2 4
+4 10 15
+3 6 9
+1 2 3
+4 5 6
+7 8 9");
+
+
+            TriangleCalculator.AdvancedGetTheNumberOfValidTriangles(input);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode2016_Day3/TriangleCalculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016_Day3
+{
+    public static class TriangleCalculator
+    {
+        public static int GetTheNumberOfValidTriangles(string input)
+        {
+            int count = 0;
+
+            string[] triangles = Regex.Split(input, "\r\n");
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                string[] triangle = Regex.Split(triangles[i], " ").Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                int[] triangleValues = new int[3];
+
+                for (int j = 0; j < 3; j++)
+                {
+                    triangleValues[j] = int.Parse(triangle[j].Trim());
+                }
+                
+                if (IsValidTriangle(triangleValues[0], triangleValues[1], triangleValues[2]))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(input);
+            Console.WriteLine("The number of valid triangles: " + count);
+
+            return count;
+        }
+
+        public static int AdvancedGetTheNumberOfValidTriangles(string input)
+        {
+            int count = 0;
+
+            string[] triangles = Regex.Split(input, "\r\n");
+            int[,] triangleValues = new int[3, triangles.Length];
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                string[] triangle = Regex.Split(triangles[i], " ").Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                triangleValues[0, i] = int.Parse(triangle[0]);
+                triangleValues[1, i] = int.Parse(triangle[1]);
+                triangleValues[2, i] = int.Parse(triangle[2]);
+            }
+            
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < triangles.Length; k += 3)
+                {
+                    if (IsValidTriangle(triangleValues[j, k], triangleValues[j, k+1], triangleValues[j, k+2]))
+                        count++;
+                }
+            }
+
+            Console.WriteLine(input);
+            Console.WriteLine("The number of valid  triangles (advance): " + count);
+
+            return count;
+        }
+
+        private static bool IsValidTriangle(int a, int b, int c)
+        {
+            bool valid = true;
+
+            if (a + b <= c || b + c <= a || a + c <= b)
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
