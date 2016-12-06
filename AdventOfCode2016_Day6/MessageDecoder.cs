bb1cd51d306bc0e8a1cb97b57620c390fdbf4356
Emil Péter Hegyi <emil.hegyi@tappointment.com>
2016-12-06 09:54:11 +0100

    Solutions for day 6.

---
+++ AdventOfCode2016_Day6/MessageDecoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016_Day6
+{
+    public static class MessageDecoder
+    {
+        public static void GetDecodedMessage(string fileName, bool mostCommon)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                var l = lines.Select(x => x[i]);
+
+                Dictionary<char, int> charCounter = new Dictionary<char, int>();
+                foreach (char item in l.Distinct())
+                {
+                    charCounter.Add(item, l.Count(x => x == item));
+                }
+
+                if (mostCommon)
+                    sb.Append(charCounter.OrderByDescending(x => x.Value).Select(x => x.Key).First());
+                else 
+                    sb.Append(charCounter.OrderBy(x => x.Value).Select(x => x.Key).First());
+
+                }
+
+                Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ AdventOfCode2016_Day6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2016_Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageDecoder.GetDecodedMessage(@"..\\..\\Test.txt", true);
+
+            MessageDecoder.GetDecodedMessage(@"..\\..\\adventOfCode2016_day6_input.txt", true);
+
+            MessageDecoder.GetDecodedMessage(@"..\\..\\Test.txt", false);
+
+            MessageDecoder.GetDecodedMessage(@"..\\..\\adventOfCode2016_day6_input.txt", false);
+
+            Console.ReadLine();
+        }
+    }
+}
